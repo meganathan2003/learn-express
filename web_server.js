8cc5a024fe20e2a1ade2239ec34af87f4814405d
meganathan/2003 <meganathan212003@gmail.com>
2024-05-14 17:45:09 +0530

    feat: Add a new js file

---
+++ web_server.js
@@ -0,0 +1,14 @@
+// import the express 
+const express = require('express');
+const app = express();
+
+const path = require('path');
+const PORT = process.env.PORT || 3500;
+
+app.get('/', (req, res) => {
+    res.send('Successfully render the page');
+})
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+
